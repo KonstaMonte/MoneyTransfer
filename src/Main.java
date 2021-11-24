04e83ed584edef96dea0e79314a389fa6cb1ea99
Alexeev Konstantin <latituderus199@gmail.com>
2021-11-24 15:14:26 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int Balance = 2_000_000_000;
+        int translation = 500_000_000;
+        int total = Balance + translation;
+        System.out.println(total);
+    }
+}
+
+
